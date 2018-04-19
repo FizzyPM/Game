c3543c03432b75f4de0929f141c39f02cb599217
FizzyPM <munjal.pranav14@gmail.com>
2018-04-20 01:51:49 +0530

    The Low-Res Update

--- play.lua
+++ play.lua
@@ -1,14 +1,22 @@
-WinWidth=1280
-WinHeight=720
+push = require 'push'
+WinHeight =720
+WinWidth =1280
+VirtualH=243
+VirtualW=432
 
 function love.load()
-	love.window.setMode(WinWidth,WinHeight,{
-		fullscreen=false,
-		resizable=false,
-		vsync=true
-		})
+	love.graphics.setDefaultFilter('nearest','nearest')
+	push:setupScreen(VirtualW,VirtualH,WinWidth,WinHeight,{fullscreen=false,resizable=false,vsync=true})
+end
+
+function love.keypressed(k)
+	if k=='escape' then
+		love.event.quit()
+	end
 end
 
 function love.draw()
-	love.graphics.printf('Hello Pong!',0,WinHeight/2-6,WinWidth,'center')
+	push:apply('start')
+	love.graphics.printf('Hello Pong!',0,VirtualH/2-6,VirtualW,'center')
+	push:apply('end')
 end
